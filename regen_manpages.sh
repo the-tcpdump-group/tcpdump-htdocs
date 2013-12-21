b4787a8dca1184a1935eb7ee5bb326d9f5bf1efc
Denis Ovsienko <infrastation@yandex.ru>
2013-12-21 13:03:55 +0400

    improve HTML anchors conditioning
    Put all sed commands into a temporary script file and call sed only once
    per HTML file. Replace most hardcoded sed commands with a few code
    blocks. Add a code block to convert 3PCAP manpages references into
    hyperlinks.

--- regen_manpages.sh
+++ regen_manpages.sh
@@ -15,35 +15,129 @@ function stripContentTypeHeader
 	tail --lines +3
 }
 
-# Convert links to non-local pages to plain text, fixup links to local pages and
-# "main contents".
 function conditionAnchors
 {
-     sed "s@<A HREF=\"$MAN2HTML_PFX\">Return to Main Contents</A>@<A HREF=\".\">Return to Main Contents</A>@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX\">man2html</A>@man2html@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?3N+ethers\">ethers</A>@ethers@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?4P+tcp\">tcp</A>@tcp@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?4P+udp\">udp</A>@udp@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?4P+ip\">ip</A>@ip@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?4+pf\">pf</A>@pf@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?8+pfconfig\">pfconfig</A>@pfconfig@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?2+select\">select</A>@select@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?2+poll\">poll</A>@poll@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?1+autoconf\">autoconf</A>@autoconf@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?1M+usermod\">usermod</A>@usermod@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?1+tcpslice\">tcpslice</A>@tcpslice@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?3+strerror\">strerror</A>@strerror@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?1+kill\">kill</A>@kill@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?1+stty\">stty</A>@stty@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?1+ps\">ps</A>@ps@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?3+strftime\">strftime</A>@strftime@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?4+bpf\">bpf</A>@bpf@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?4P+nit\">nit</A>@nit@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?7+pcap-linktype\"@<A HREF=\"$WEBSITE_PFX/pcap-linktype.7.html\"@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?7+pcap-tstamp\"@<A HREF=\"$WEBSITE_PFX/pcap-tstamp.7.html\"@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?7+pcap-filter\"@<A HREF=\"$WEBSITE_PFX/pcap-filter.7.html\"@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?5+pcap-savefile\"@<A HREF=\"$WEBSITE_PFX/pcap-savefile.5.html\"@g" | \
-     sed "s@<A HREF=\"$MAN2HTML_PFX?1+tcpdump\"@<A HREF=\"/tcpdump_man.html\"@g"
+	sed --file="$SEDFILE"
+}
+
+function write_sedfile
+{
+	local mansection mantopic manfile
+
+	# Fixup custom links.
+	cat >$SEDFILE <<ENDOFFILE
+s@<A HREF="$MAN2HTML_PFX">Return to Main Contents</A>@<A HREF="$WEBSITE_PFX">Return to Main Contents</A>@g
+s@<A HREF="$MAN2HTML_PFX">man2html</A>@man2html@g
+ENDOFFILE
+
+	# Convert links to non-local pages to plain text.
+	while read mansection mantopic; do
+		echo "s@<A HREF=\"$MAN2HTML_PFX?${mansection}+${mantopic}\">$mantopic</A>@$mantopic@g"
+	done >>$SEDFILE <<ENDOFLIST
+3N	ethers
+4P	tcp
+4P	udp
+4P	ip
+4	pf
+8	pfconfig
+2	select
+2	poll
+1	autoconf
+1M	usermod
+1	tcpslice
+3	strerror
+1	kill
+1	stty
+1	ps
+3	strftime
+4	bpf
+4P	nit
+ENDOFLIST
+
+	# Fixup links to local pages, part 1.
+	while read mantopic manfile; do
+		echo "s@<A HREF=\"${MAN2HTML_PFX}?${mantopic}\"@<A HREF=\"$manfile\"@g"
+	done >>$SEDFILE <<ENDOFLIST
+7+pcap-linktype		$WEBSITE_PFX/pcap-linktype.7.html
+7+pcap-tstamp		$WEBSITE_PFX/pcap-tstamp.7.html
+7+pcap-filter		$WEBSITE_PFX/pcap-filter.7.html
+5+pcap-savefile		$WEBSITE_PFX/pcap-savefile.5.html
+1+tcpdump		/tcpdump_man.html
+ENDOFLIST
+
+	# Fixup links to local pages, part 2.
+	while read mantopic manfile; do
+		[ "$manfile" = "" ] && manfile=$mantopic
+		manfile="${WEBSITE_PFX}/${manfile}.3pcap.html"
+		echo "s@$mantopic(3PCAP)@<A HREF='$manfile'>$mantopic</A>(3PCAP)@g"
+		echo "s@<B>$mantopic</B>(3PCAP)@<A HREF='$manfile'><B>$mantopic</B></A>(3PCAP)@g"
+	done >>$SEDFILE <<ENDOFLIST
+pcap
+pcap_activate
+pcap_breakloop
+pcap_can_set_rfmon
+pcap_close
+pcap_compile
+pcap_create
+pcap_datalink
+pcap_datalink_name_to_val
+pcap_datalink_val_to_description				pcap_datalink_val_to_name
+pcap_datalink_val_to_name
+pcap_dispatch							pcap_loop
+pcap_dump
+pcap_dump_close
+pcap_dump_file
+pcap_dump_flush
+pcap_dump_fopen							pcap_dump_open
+pcap_dump_ftell
+pcap_dump_open
+pcap_file
+pcap_findalldevs
+pcap_fopen_offline						pcap_open_offline
+pcap_fopen_offline_with_tstamp_precision			pcap_open_offline
+pcap_freealldevs						pcap_findalldevs
+pcap_freecode
+pcap_free_datalinks						pcap_list_datalinks
+pcap_free_tstamp_types						pcap_list_tstamp_types
+pcap_geterr
+pcap_getnonblock						pcap_setnonblock
+pcap_get_selectable_fd
+pcap_inject
+pcap_is_swapped
+pcap_lib_version
+pcap_list_datalinks
+pcap_list_tstamp_types
+pcap_lookupdev
+pcap_lookupnet
+pcap_loop
+pcap_major_version
+pcap_minor_version						pcap_major_version
+pcap_next_ex
+pcap_next							pcap_next_ex
+pcap_offline_filter
+pcap_open_dead
+pcap_open_dead_with_tstamp_precision				pcap_open_dead
+pcap_open_offline
+pcap_open_offline_with_tstamp_precision				pcap_open_offline
+pcap_perror							pcap_geterr
+pcap_sendpacket							pcap_inject
+pcap_set_buffer_size
+pcap_set_datalink
+pcap_setdirection
+pcap_setfilter
+pcap_setnonblock
+pcap_set_promisc
+pcap_set_rfmon
+pcap_set_snaplen
+pcap_set_timeout
+pcap_set_tstamp_type
+pcap_snapshot
+pcap_stats
+pcap_statustostr
+pcap_tstamp_name_to_val						pcap_datalink
+pcap_tstamp_type_val_to_description				pcap_tstamp_type_val_to_name
+pcap_tstamp_type_val_to_name
+ENDOFLIST
 }
 
 function produceHTML
@@ -78,6 +172,9 @@ if [ "$COLS" != "80" ]; then
 	exit 1
 fi
 
+SEDFILE=`mktemp --tmpdir manpages_sedfile.XXXXXX`
+write_sedfile
+
 produceHTML ../tcpdump/tcpdump.1 tcpdump_man.html
 produceHTML ../libpcap/pcap.3pcap pcap3_man.html
 
@@ -95,3 +192,5 @@ for f in ../libpcap/*.3pcap ../libpcap/pcap-config.1; do
 	produceTXT $f manpages/`basename $f`.txt
 	produceHTML $f manpages/`basename $f`.html
 done
+
+rm -f $SEDFILE
