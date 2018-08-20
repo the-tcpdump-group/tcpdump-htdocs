09e09c032bd7471c0980baabf1ebb70c868a3298
Denis Ovsienko <denis@ovsienko.info>
2018-08-20 21:35:59 +0100

    handle the rpcapd man page in the script too

--- regen_manpages.sh
+++ regen_manpages.sh
@@ -264,11 +264,13 @@ updateOutputFiles()
 	produceTXT ../libpcap/pcap-linktype.manmisc manpages/pcap-linktype.7.txt
 	produceTXT ../libpcap/pcap-savefile.manfile manpages/pcap-savefile.5.txt
 	produceTXT ../libpcap/pcap-tstamp.manmisc manpages/pcap-tstamp.7.txt
+	produceTXT ../libpcap/rpcapd/rpcapd.manadmin manpages/rpcapd.1.txt
 
 	produceHTML ../libpcap/pcap-filter.manmisc "$sedfile" manpages/pcap-filter.7.html
 	produceHTML ../libpcap/pcap-linktype.manmisc "$sedfile" manpages/pcap-linktype.7.html
 	produceHTML ../libpcap/pcap-savefile.manfile "$sedfile" manpages/pcap-savefile.5.html
 	produceHTML ../libpcap/pcap-tstamp.manmisc "$sedfile" manpages/pcap-tstamp.7.html
+	produceHTML ../libpcap/rpcapd/rpcapd.manadmin "$sedfile" manpages/rpcapd.1.html
 
 	for f in ../libpcap/*.3pcap; do
 		[ "`known3PCAPFile $f`" = 'no' ] && echo "WARNING: file $f is not in the 3PCAP map"
