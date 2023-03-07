87233e9b904531d59a631f7bc4d50cd9844e9905
Denis Ovsienko <denis@ovsienko.info>
2023-03-07 23:10:20 +0000

    Refine bold-to-code substitution further.
    Require no other tags between <B> and </B>, and allow parentheses
    around.  This starts to produce acceptable result for
    pcap-tstamp.manmisc.
    
    [skip ci]

--- regen_man_pages.sh
+++ regen_man_pages.sh
@@ -269,8 +269,8 @@ definitelyBoldToCode()
 	# not look well.  This seems to be the easiest approach since sed
 	# regexps do not support negative matching and this function is the
 	# only source of <CODE> tags in the page.
-	sed -E '/^<H2 .+>DESCRIPTION/,$s@^<DT><B>(.+)</B>@<DT><CODE>\1</CODE>@' |
-		sed -E '/^<H2 .+>DESCRIPTION/,$s@^<B>(.+)</B>([,.;]?)$@<CODE>\1</CODE>\2@' |
+	sed -E '/^<H2 .+>DESCRIPTION/,$s@^<DT><B>([^<>]+)</B>@<DT><CODE>\1</CODE>@' |
+		sed -E '/^<H2 .+>DESCRIPTION/,$s@^([(]?<)B(>[^<>]+</)B(>[,.;)]?)$@\1CODE\2CODE\3@' |
 		sed -E 's@<CODE>(NOT|not|0|1|-1)</CODE>@<B>\1</B>@'
 }
 
@@ -292,7 +292,6 @@ maybeBoldToCode()
 		pcap-filter
 		pcap_get_required_select_timeout
 		pcap_lookupdev
-		pcap-tstamp
 		rpcapd
 		rpcapd-config
 		tcpdump
