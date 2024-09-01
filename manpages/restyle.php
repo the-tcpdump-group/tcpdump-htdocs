c3933842593aed2e148e323a5c492031d24f89fb
Denis Ovsienko <denis@ovsienko.info>
2024-09-01 14:55:16 +0100

    restyle.php: Fix a PHP 8.3 warning.
    Deprecated: Using ${var} in strings is deprecated, use {$var} instead in
    manpages/restyle.php on line 174

--- manpages/restyle.php
+++ manpages/restyle.php
@@ -171,7 +171,7 @@ if (! array_key_exists ('REDIRECT_URL', $_SERVER))
 if ($uri_path != $_SERVER['REDIRECT_URL'])
 {
 	# The value is just the path, so the reference is relative.
-	header ("Location: ${_SERVER['REDIRECT_URL']}", TRUE, 301);
+	header ("Location: {$_SERVER['REDIRECT_URL']}", TRUE, 301);
 	exit;
 }
 
